45060e8448d4be555a4ea99cfc1e7595ae035dc8
Devansh Behl <heydevanshbehl@gmail.com>
2024-08-15 00:30:12 +0530

    add : contact us

---
+++ app/contact/page.tsx
@@ -0,0 +1,14 @@
+import Navbar from "@/components/navbar/navbar";
+import ContactForm from "./contact-form";
+
+
+const Contact = () => {
+    return ( 
+    <div>
+     <Navbar />
+     <ContactForm />
+    </div> 
+    );
+}
+ 
+export default Contact;
